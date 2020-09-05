b18ac626e83aaf31c5d4f61a154ef5325f3e2420
sheshihao <sheshihao@mbcloud.com>
2020-09-05 11:48:44 +0800

    进行注释

--- Week_06/最长公共子序列.js
+++ Week_06/最长公共子序列.js
@@ -1,5 +1,7 @@
 var longestCommonSubsequence = function(text1, text2) {
   let arr = [];
+
+  // 下面赋初始值可以简化 let dp = Array.from(new Array(n+1),() => new Array(m+1).fill(0));
   for(let i = 0;i<=text1.length;i++){
       let row = new Array(text2.length + 1);
       row[0] = 0;
@@ -17,6 +19,7 @@ var longestCommonSubsequence = function(text1, text2) {
             // 这个不能直接用preMax 会进行累计
             arr[i][j] = arr[i-1][j-1] + 1;
           }else{
+            // 这里没有必要对比 arr[i-1][j-1] ==》 dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
             let preMax = Math.max(arr[i-1][j-1],arr[i][j-1],arr[i-1][j]);
             arr[i][j] = preMax;
           }
